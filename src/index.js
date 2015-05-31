0bd01ec350e349e401503143bb195485c4489275
Manuel Guilbault <manuel.guilbault@gmail.com>
2015-05-31 14:12:11 +0200

    feat(validation): add view startegy for aurelia-validation plugin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 ﻿export {MaterializeSelect} from './select';
 export {MaterializeSideNav} from './side-nav';
+export {MaterializeValidationViewStrategy} from './validation-view-strategy';
 
 export function configure(aurelia) {
   aurelia.globalizeResources(
