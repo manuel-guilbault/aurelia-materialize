a779b5824b15203fe245d0a4f3b575696ca5d591
Manuel Guilbault <manuel.guilbault@gmail.com>
2015-06-07 17:24:43 +0200

    test(side-nav): add tests for side-nav

---
+++ test/side-nav.spec.js
@@ -0,0 +1,42 @@
+﻿import $ from 'jquery';
+import {MaterializeSideNav} from '../src/side-nav';
+
+describe('side-nav', () => {
+
+  var anchor, sideNav, callOptions;
+
+  beforeEach(() => {
+    spyOn($.fn, 'sideNav');
+    anchor = document.createElement('a');
+    sideNav = new MaterializeSideNav(anchor);
+  });
+
+  function expectSideNavCallWithOptions(expectedOptions) {
+    expect($.fn.sideNav.calls.count()).toEqual(1);
+
+    var calledOptions = $.fn.sideNav.calls.argsFor(0)[0];
+    for (var option in expectedOptions) {
+      expect(calledOptions[option]).toEqual(expectedOptions[option]);
+    }
+  }
+
+  it('should create sideNav upon attached', () => {
+    sideNav.attached();
+    
+    expectSideNavCallWithOptions({});
+  });
+
+  it('should pass options to sideNav upon attached', () => {
+    sideNav.menuWidth = 600;
+    sideNav.edge = 'right';
+    sideNav.closeOnClick = false;
+
+    sideNav.attached();
+    
+    expectSideNavCallWithOptions({
+      menuWidth: 600,
+      edge: 'right',
+      closeOnClick: false
+    });
+  });
+});
